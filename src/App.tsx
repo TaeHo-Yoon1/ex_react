cb61ffdc49da778dc194469a79a4bbeee4fb48b6
woo4826 <woo4826@gmail.com>
2024-09-30 13:20:18 +0900

    MOD: SVG -> SVGG modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,55 @@
-import React, { useRef, useEffect } from 'react';
-import { dia, shapes, ui } from 'react-joint-js-plus';
-import './App.css';
+import { useEffect, useRef } from "react";
+import { dia, shapes, ui } from "react-joint-js-plus";
+import "./App.css";
 
 function App() {
   const canvas: any = useRef(null);
 
   useEffect(() => {
-      const graph = new dia.Graph();
-      const paper = new dia.Paper({
-          model: graph,
-          background: {
-              color: '#F8F9FA',
-          },
-          frozen: true,
-          async: true,
-          sorting: dia.Paper.sorting.APPROX,
-          cellViewNamespace: shapes
-      });
+    const graph = new dia.Graph();
+    const paper = new dia.Paper({
+      model: graph,
+      background: {
+        color: "#F8F9FA",
+      },
+      frozen: true,
+      async: true,
+      sorting: dia.Paper.sorting.APPROX,
+      cellViewNamespace: shapes,
+    });
 
-      const scroller = new ui.PaperScroller({
-          paper,
-          autoResizePaper: true,
-          cursor: 'grab'
-      });
+    const scroller = new ui.PaperScroller({
+      paper,
+      autoResizePaper: true,
+      cursor: "grab",
+    });
 
-      canvas.current.appendChild(scroller.el);
-      scroller.render().center();
+    canvas.current.appendChild(scroller.el);
+    scroller.render().center();
 
-      const rect = new shapes.standard.Rectangle({
-          position: { x: 100, y: 100 },
-          size: { width: 100, height: 50 },
-          attrs: {
-              label: {
-                  text: 'Hello World'
-              }
-          }
-      });
+    const rect = new shapes.standard.Rectangle({
+      position: { x: 100, y: 100 },
+      size: { width: 100, height: 50 },
+      attrs: {
+        label: {
+          text: "Hello World3",
+          fill: "#3498db",
+          stroke: "#2980b0",
+          strokeWidth: 1,
+        },
+      },
+    });
 
-      graph.addCell(rect);
-      paper.unfreeze();
+    graph.addCell(rect);
+    paper.unfreeze();
 
-      return () => {
-          scroller.remove();
-          paper.remove();
-      };
-
+    return () => {
+      scroller.remove();
+      paper.remove();
+    };
   }, []);
 
-  return (
-    <div className="canvas" ref={canvas}/>
-  );
+  return <div className="canvas" ref={canvas} />;
 }
 
 export default App;
